b6c7de9b57552b726ddaa246fc0a7580ed61fe55
EricDosReis <ericdosreis.personal@gmail.com>
2019-09-23 11:43:21 -0300

    Create updateAmount action in Cart reducer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -17,6 +17,7 @@ export default function cart(state = [], action) {
           });
         }
       });
+
     case '@cart/REMOVE':
       return produce(state, draft => {
         const productIndex = draft.findIndex(({ id }) => id === action.id);
@@ -25,6 +26,20 @@ export default function cart(state = [], action) {
           draft.splice(productIndex, 1);
         }
       });
+
+    case '@cart/UPDATE_AMOUNT': {
+      if (action.amount <= 0) {
+        return state;
+      }
+
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(({ id }) => id === action.id);
+
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      });
+    }
     default:
       return state;
   }
